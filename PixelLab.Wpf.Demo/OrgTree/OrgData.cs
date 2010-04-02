6220fbc22eb620a195e65b41e4f1f7bd57fe4948
Kevin Moore <kevin@thinkpixellab.com>
2010-04-02 20:10:10 +0000

    OrgTree demo - Much more cleaner
    git-svn-id: https://bot.svn.codeplex.com/svn/trunk@44775 2327b42d-5241-43d6-9e2a-de5ac946f064

--- PixelLab.Wpf.Demo/OrgTree/OrgData.cs
+++ PixelLab.Wpf.Demo/OrgTree/OrgData.cs
@@ -7,106 +7,105 @@ using System.Windows.Input;
 using PixelLab.Common;
 
 namespace PixelLab.Wpf.Demo.OrgTree {
-    public abstract class OrgItem : INotifyPropertyChanged {
-        protected OrgItem(string name, IEnumerable<OrgItem> children) {
-            Util.RequireNotNullOrEmpty(name, "name");
+  public abstract class OrgItem : INotifyPropertyChanged {
+    protected OrgItem(string name, IEnumerable<OrgItem> children) {
+      Util.RequireNotNullOrEmpty(name, "name");
 
-            if (children == null) {
-                children = Enumerable.Empty<OrgItem>();
-            }
+      if (children == null) {
+        children = Enumerable.Empty<OrgItem>();
+      }
 
-            Name = name;
-            _children = new ReadOnlyCollection<OrgItem>(children.ToArray());
-            _children.ForEach(child => child.SetParent(this));
-            _isVisible = false;
-            _isExpanded = false;
-        }
-
-        public string Name { get; private set; }
-
-        public bool IsVisible {
-            get { return _isVisible; }
-            set {
-                if (value != _isVisible) {
-                    _isVisible = value;
-                    OnPropertyChanged("IsVisible");
-                }
-            }
-        }
-
-        public bool IsExpanded {
-            get { return _isExpanded; }
-            set {
-                if (value != _isExpanded) {
-                    _isExpanded = value;
-                    OnPropertyChanged("IsExpanded");
-                }
-            }
-        }
-
-        public OrgItem Parent { get { return _parent; } }
-
-        public ReadOnlyCollection<OrgItem> Children {
-            get { return _children; }
-        }
-
-        public void RequestExpand() {
-            if (_parent != null) {
-                _parent.IsExpanded = true;
-                _parent.RequestExpand();
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args) {
-            var handler = PropertyChanged;
-            if (handler != null) {
-                handler(this, args);
-            }
-        }
-
-        protected void OnPropertyChanged(string propertyName) {
-            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
-        }
-
-        public override string ToString() {
-            return String.Format("OrgItem: '{0}'", Name);
-        }
-
-        #region private instance impl
-
-        private void SetParent(OrgItem parent) {
-            _parent = parent;
-        }
-
-        private bool _isVisible;
-        private bool _isExpanded;
-        private readonly ReadOnlyCollection<OrgItem> _children;
-        private OrgItem _parent;
-
-        #endregion
-
-        public static ICommand ToggleVisibilityCommand { get { return s_toggleVisibilityCommand.Command; } }
-
-        private static readonly CommandWrapper<OrgItem> s_toggleVisibilityCommand =
-            new CommandWrapper<OrgItem>(oi => {
-                oi.IsVisible = !oi.IsVisible;
-                if (oi.IsVisible) {
-                    oi.RequestExpand();
-                }
-            });
+      Name = name;
+      m_children = new ReadOnlyCollection<OrgItem>(children.ToArray());
+      m_children.ForEach(child => child.SetParent(this));
+      m_isVisible = true;
     }
 
-    public class Company : OrgItem {
-        public Company(string name, IEnumerable<OrgItem> children) : base(name, children) { }
+    public string Name { get; private set; }
+
+    public bool IsVisible {
+      get { return m_isVisible; }
+      set {
+        if (value != m_isVisible) {
+          m_isVisible = value;
+          OnPropertyChanged("IsVisible");
+        }
+      }
     }
 
-    public class Department : OrgItem {
-        public Department(string name, IEnumerable<OrgItem> children) : base(name, children) { }
+    public bool IsExpanded {
+      get { return m_isExpanded; }
+      set {
+        if (value != m_isExpanded) {
+          m_isExpanded = value;
+          OnPropertyChanged("IsExpanded");
+        }
+      }
     }
 
-    public class Employee : OrgItem {
-        public Employee(string name, IEnumerable<OrgItem> children) : base(name, children) { }
+    public OrgItem Parent { get { return m_parent; } }
+
+    public ReadOnlyCollection<OrgItem> Children {
+      get { return m_children; }
     }
+
+    public void RequestExpand() {
+      if (m_parent != null) {
+        m_parent.IsExpanded = true;
+        m_parent.RequestExpand();
+      }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(PropertyChangedEventArgs args) {
+      var handler = PropertyChanged;
+      if (handler != null) {
+        handler(this, args);
+      }
+    }
+
+    protected void OnPropertyChanged(string propertyName) {
+      OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+    }
+
+    public override string ToString() {
+      return String.Format("OrgItem: '{0}'", Name);
+    }
+
+    #region private instance impl
+
+    private void SetParent(OrgItem parent) {
+      m_parent = parent;
+    }
+
+    private bool m_isVisible;
+    private bool m_isExpanded;
+    private readonly ReadOnlyCollection<OrgItem> m_children;
+    private OrgItem m_parent;
+
+    #endregion
+
+    public static ICommand ToggleVisibilityCommand { get { return s_toggleVisibilityCommand.Command; } }
+
+    private static readonly CommandWrapper<OrgItem> s_toggleVisibilityCommand =
+        new CommandWrapper<OrgItem>(oi => {
+          oi.IsVisible = !oi.IsVisible;
+          if (oi.IsVisible) {
+            oi.RequestExpand();
+          }
+        });
+  }
+
+  public class Company : OrgItem {
+    public Company(string name, IEnumerable<OrgItem> children) : base(name, children) { }
+  }
+
+  public class Department : OrgItem {
+    public Department(string name, IEnumerable<OrgItem> children) : base(name, children) { }
+  }
+
+  public class Employee : OrgItem {
+    public Employee(string name, IEnumerable<OrgItem> children) : base(name, children) { }
+  }
 }
--- PixelLab.Wpf.Demo/OrgTree/OrgTreePage.xaml.cs
+++ PixelLab.Wpf.Demo/OrgTree/OrgTreePage.xaml.cs
@@ -1,37 +1,37 @@
 ﻿using System.Windows.Controls;
 
-namespace PixelLab.Wpf.Demo.OrgTree{
-    public partial class OrgTreePage : Page{
-        public OrgTreePage(){
-            InitializeComponent();
-            _tree.ItemsSource = OrgViewItem.GetViewArray(GetDemoData());
-        }
-
-        private static OrgItem[] GetDemoData(){
-            return new OrgItem[]{
-                new Company("Acme", new OrgItem[]{
-                    new Department("Sales", new OrgItem[]{
-                        new Employee("Bill Jones", null),
-                        new Employee("Bob Jones", null),
-                        new Employee("Beth Jones", null)
-                    }),
-                    new Department("Service", new OrgItem[]{
-                        new Employee("Bill Smith", null),
-                        new Employee("Bob Smith", null),
-                        new Employee("Beth Smith", null)
-                    }),
-                    new Department("Marketing", new OrgItem[]{
-                        new Employee("Bill Adams", null),
-                        new Employee("Bob Adams", null),
-                        new Employee("Beth Adams", null)
-                    }),
-                    new Department("Engineering", new OrgItem[]{
-                        new Employee("Bill Obama", null),
-                        new Employee("Bob Obama", null),
-                        new Employee("Beth Obama", null)
-                    })
-                })
-            };
-        }
+namespace PixelLab.Wpf.Demo.OrgTree {
+  public partial class OrgTreePage : Page {
+    public OrgTreePage() {
+      InitializeComponent();
+      _tree.ItemsSource = OrgViewItem.GetViewArray(GetDemoData());
     }
+
+    private static OrgItem[] GetDemoData() {
+      return new OrgItem[]{
+        new Company("Acme", new OrgItem[]{
+          new Department("Sales", new OrgItem[]{
+              new Employee("Bill Jones", null),
+              new Employee("Bob Jones", null),
+              new Employee("Beth Jones", null)
+          }),
+          new Department("Service", new OrgItem[]{
+              new Employee("Bill Smith", null),
+              new Employee("Bob Smith", null),
+              new Employee("Beth Smith", null)
+          }),
+          new Department("Marketing", new OrgItem[]{
+              new Employee("Bill Adams", null),
+              new Employee("Bob Adams", null),
+              new Employee("Beth Adams", null)
+          }),
+          new Department("Engineering", new OrgItem[]{
+              new Employee("Bill Obama", null),
+              new Employee("Bob Obama", null),
+              new Employee("Beth Obama", null)
+          })
+          })
+      };
+    }
+  }
 }
