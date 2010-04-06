2fe5612374c489abba92962282608299a327a465
Kevin Moore <kevin@thinkpixellab.com>
2010-04-06 17:46:22 +0000

    Tightened up navigation logic for external URLs
    Made link in DateControlsPage to wpf.codeplex.com an actual link
    Fixed a type-o
    Standarized on Bag *of* Tricks
    
    
    git-svn-id: https://bot.svn.codeplex.com/svn/trunk@45213 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ PixelLab.Wpf.Demo/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+
+namespace PixelLab.Wpf.Demo {
+  public partial class MainPage : Page {
+    public MainPage() {
+      InitializeComponent();
+
+      m_contentFrame.Navigating += (sender, e) => {
+        var uri = e.Uri.ToString();
+        if (uri.StartsWith("http://")) {
+          Process.Start(uri);
+          e.Cancel = true;
+        }
+      };
+
+    }
+  }
+}
