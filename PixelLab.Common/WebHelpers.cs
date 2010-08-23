0b3e242314dffde9dd7db1c7c7e226672ba43bc5
Kevin Moore <kevin@thinkpixellab.com>
2010-08-23 19:36:02 +0000

    Added Stream and Web Helpers
    git-svn-id: https://bot.svn.codeplex.com/svn/trunk@59679 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ PixelLab.Common/WebHelpers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+#if WP7
+using Microsoft.Phone.Reactive;
+#endif
+
+namespace PixelLab.Common {
+  public static class WebHelpers {
+    public static IObservable<string> GetResponseAsStringAsync(this WebRequest webRequest) {
+      return from response in webRequest.GetResponseAsync() select response.ReadAsString();
+    }
+
+    public static IObservable<byte[]> GetResponseAsBytesAsync(this WebRequest webRequest) {
+      return from response in webRequest.GetResponseAsync() select response.ReadAsBytes();
+    }
+
+    public static IObservable<WebResponse> GetResponseAsync(this WebRequest webRequest) {
+      return Observable.FromAsyncPattern<WebResponse>(webRequest.BeginGetResponse, webRequest.EndGetResponse)();
+    }
+
+    public static string ReadAsString(this WebResponse response) {
+#if DEBUG
+      System.Diagnostics.Debug.WriteLine("Reading URI: {0}", ((HttpWebResponse)response).ResponseUri);
+#endif
+      using (var stream = response.GetResponseStream()) {
+        return stream.ReadAllAsString();
+      }
+    }
+
+    public static byte[] ReadAsBytes(this WebResponse response) {
+#if DEBUG
+      System.Diagnostics.Debug.WriteLine("Reading URI: {0}", ((HttpWebResponse)response).ResponseUri);
+#endif
+      using (var stream = response.GetResponseStream()) {
+        return stream.ReadAllAsBytes();
+      }
+    }
+
+
+  }
+}
