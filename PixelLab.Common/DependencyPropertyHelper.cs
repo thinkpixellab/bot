c35075fd392cbf16db160ca18627501aab1a5aa8
Kevin Moore <kevin@thinkpixellab.com>
2010-09-10 05:57:37 +0000

    DependencyPropertyHelper and EnumConverter -> both a lot of fun
    Lots of good uses all over the BOT...eventually
    
    git-svn-id: https://bot.svn.codeplex.com/svn/trunk@60724 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ PixelLab.Common/DependencyPropertyHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+
+namespace PixelLab.Common {
+  public static class DependencyPropertyHelper {
+    public static PropertyChangedCallback GetTypedDPChangedHandler<TElement, TProperty>(Action<TElement, TProperty, TProperty> handler) where TElement : DependencyObject {
+      return new PropertyChangedCallback((element, args) => {
+        handler((TElement)element, (TProperty)args.NewValue, (TProperty)args.OldValue);
+      });
+    }
+
+    public static PropertyMetadata GetTypePropertyMetadata<TElement, TProperty>(Action<TElement, TProperty, TProperty> handler) where TElement : DependencyObject {
+      return new PropertyMetadata(GetTypedDPChangedHandler<TElement, TProperty>(handler));
+    }
+
+    public static DependencyProperty Register<TElement, TProperty>(string name, Action<TElement, TProperty, TProperty> changeHandler = null) where TElement : DependencyObject {
+      PropertyMetadata metadata;
+      if (changeHandler == null) {
+        metadata = null;
+      }
+      else {
+        metadata = GetTypePropertyMetadata(changeHandler);
+      }
+
+      return DependencyProperty.Register(name, typeof(TProperty), typeof(TElement), metadata);
+
+    }
+
+    public static DependencyProperty RegisterAttached<TOwner, TTarget, TProperty>(string name, Action<TTarget, TProperty, TProperty> changeHandler = null) where TTarget : DependencyObject {
+      PropertyMetadata metadata;
+      if (changeHandler == null) {
+        metadata = null;
+      }
+      else {
+        metadata = GetTypePropertyMetadata(changeHandler);
+      }
+
+      return DependencyProperty.Register(name, typeof(TProperty), typeof(TOwner), metadata);
+    }
+
+  }
+}
