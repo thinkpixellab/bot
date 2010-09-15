d657f4ad85275d2c54d27ea4fb7bf6d1051e5778
Kevin Moore <kevin@thinkpixellab.com>
2010-09-15 23:25:11 +0000

    Removed compiled option from EnumConverter regex -> works in SL4
    git-svn-id: https://bot.svn.codeplex.com/svn/trunk@61022 2327b42d-5241-43d6-9e2a-de5ac946f064

--- PixelLab.Common/EnumConverter.cs
+++ PixelLab.Common/EnumConverter.cs
@@ -78,7 +78,7 @@ namespace PixelLab.Common {
       }
     }
 
-    private readonly Regex m_regex = new Regex(s_match, RegexOptions.CultureInvariant | RegexOptions.Compiled | RegexOptions.Singleline);
+    private readonly Regex m_regex = new Regex(s_match, RegexOptions.CultureInvariant | RegexOptions.Singleline);
 
     private static readonly string s_match = string.Format("^({0})->({1})(,({2}))?$", c_validName, c_validName, c_validName);
     private const string c_validName = "[a-zA-Z_][a-zA-Z0-9_]*";
