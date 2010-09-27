be71eec2c2addb84b14f42548c865c0ce406a4c8
Kevin Moore <kevin@thinkpixellab.com>
2010-09-27 20:18:11 +0000

    Centralizing code for Demo loading
    git-svn-id: https://bot.svn.codeplex.com/svn/trunk@61600 2327b42d-5241-43d6-9e2a-de5ac946f064

--- PixelLab.SL.Demo/Core/DemoMetadata.cs
+++ PixelLab.SL.Demo/Core/DemoMetadata.cs
@@ -1,4 +1,11 @@
-﻿using System.ComponentModel.Composition.Primitives;
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using PixelLab.Common;
+using System.Diagnostics.Contracts;
 
 namespace PixelLab.SL.Demo.Core {
   public class DemoMetadata {
@@ -11,6 +18,33 @@ namespace PixelLab.SL.Demo.Core {
     public string Name { get; private set; }
     public ExportDefinition ExportDefinition { get; private set; }
     public ComposablePartDefinition PartDefinition { get; private set; }
+
+    public FrameworkElement CreateElement() {
+      return (FrameworkElement)PartDefinition.CreatePart().GetExportedValue(ExportDefinition);
+    }
+
+    public static IList<DemoMetadata> GetDemos(Assembly sourceAssembly, string firstName = null) {
+      Contract.Requires(sourceAssembly != null);
+
+      var catalog = new AssemblyCatalog(sourceAssembly);
+      var items = (from part in catalog.Parts
+                   from definition in part.ExportDefinitions
+                   where definition.ContractName == DemoMetadataAttribute.DemoContractName
+                   select new DemoMetadata(definition.Metadata["Name"] as string, definition, part)
+                   )
+                   .OrderBy(_ => _.Name)
+                   .ToList();
+
+      if (firstName != null) {
+        var welcome = items.Where(metadata => metadata.Name.Equals(firstName)).FirstOrDefault();
+        if (welcome != null) {
+          items.Remove(welcome);
+          items.Insert(0, welcome);
+        }
+      }
+
+      return items.ToReadOnlyCollection();
+    }
 
   }
 }
--- PixelLab.SL.Demo/DemoHost.xaml.cs
+++ PixelLab.SL.Demo/DemoHost.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.ComponentModel.Composition.Hosting;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 using PixelLab.SL.Demo.Core;
 
 namespace PixelLab.SL.Demo {
@@ -10,33 +6,12 @@ namespace PixelLab.SL.Demo {
     public DemoHost() {
       InitializeComponent();
 
-
-      var catalog = new AssemblyCatalog(typeof(App).Assembly);
-      var items = (from part in catalog.Parts
-                   from definition in part.ExportDefinitions
-                   where definition.ContractName == DemoMetadataAttribute.DemoContractName
-                   select new DemoMetadata(definition.Metadata["Name"] as string, definition, part)).ToList();
-
-      var welcome = items.Where(tuple => tuple.Name.Equals("Welcome", StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
-      if (welcome != null) {
-        items.Remove(welcome);
-        items.Insert(0, welcome);
-      }
-
-      m_items.ItemsSource = items;
+      m_items.ItemsSource = DemoMetadata.GetDemos(typeof(DemoHost).Assembly, "Welcome");
 
       m_items.SelectionChanged += (sender, args) => {
         var item = m_items.SelectedItem as DemoMetadata;
         if (item != null) {
-
-          var element = item.PartDefinition.CreatePart().GetExportedValue(item.ExportDefinition) as FrameworkElement;
-          if (element == null) {
-            m_container.Child = new TextBlock() { Text = "Error loading component" };
-          }
-          else {
-            m_container.Child = element;
-          }
-
+          m_container.Child = item.CreateElement();
         }
 
       };
