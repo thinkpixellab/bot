4bafeccc4fe22f28261e3174eba90746bbab70bd
Kevin Moore <kevin@thinkpixellab.com>
2010-09-27 19:55:54 +0000

    Oops...including the metadata class
    git-svn-id: https://bot.svn.codeplex.com/svn/trunk@61598 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ PixelLab.SL.Demo/Core/DemoMetadata.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.Composition.Primitives;
+
+namespace PixelLab.SL.Demo.Core {
+  public class DemoMetadata {
+    public DemoMetadata(string name, ExportDefinition definition, ComposablePartDefinition partDefinition) {
+      Name = name;
+      ExportDefinition = definition;
+      PartDefinition = partDefinition;
+    }
+
+    public string Name { get; private set; }
+    public ExportDefinition ExportDefinition { get; private set; }
+    public ComposablePartDefinition PartDefinition { get; private set; }
+
+  }
+}
