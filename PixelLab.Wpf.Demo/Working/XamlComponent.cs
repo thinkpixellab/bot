45d80bd698fb8a3c7c9869945dcd535f333af9f1
Kevin Moore <kevin@thinkpixellab.com>
2010-09-28 23:52:20 +0000

    Working on some crazy Xaml fun...
    git-svn-id: https://bot.svn.codeplex.com/svn/trunk@61642 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ PixelLab.Wpf.Demo/Working/XamlComponent.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Windows;
+using System.Windows.Baml2006;
+using System.Xaml;
+using PixelLab.Common;
+using PixelLab.Wpf.Demo;
+
+namespace PixelLab.Working {
+  public static class XamlComponent {
+
+    private static void foo() {
+
+      var assembly = typeof(App).Assembly;
+
+      var resourceSets = from resourceName in assembly.GetManifestResourceNames()
+                         let resourceStream = assembly.GetManifestResourceStream(resourceName)
+                         select new ResourceSet(resourceStream);
+
+      var bamlEntries = from set in resourceSets
+                        from entry in set.Cast<DictionaryEntry>()
+                        where entry.Key is string
+                        where entry.Value is Stream
+                        let value = new { Name = (string)entry.Key, Stream = (Stream)entry.Value }
+                        where value.Name.EndsWith(".baml")
+                        select value;
+
+      var rootTypes = from entry in bamlEntries
+                      where entry.Name.Contains("kevin")
+                      let reader = new Baml2006Reader(entry.Stream)
+                      let type = (from myReader in reader.Enumerate()
+                                  where myReader.NodeType == System.Xaml.XamlNodeType.StartObject
+                                  select new { myReader.Type, Name = getDemoName(myReader) })
+                                            .FirstOrDefault()
+                      where type != null
+                      where type.Type.UnderlyingType != null
+                      where type.Type.UnderlyingType.IsSubclassOf(typeof(FrameworkElement))
+                      let result = new { entry.Name, entry.Stream, type.Type, DemoName = type.Name }
+                      where result.DemoName != null
+                      select result;
+
+      rootTypes.ForEach(entry => {
+        var path = entry.Name.Replace(".baml", ".xaml");
+        path = string.Format("/{0};component/{1}", assembly.GetName().Name, path);
+
+        var uri = new Uri(path, UriKind.Relative);
+
+        if (path.Contains("kevin") || path.Contains("main")) {
+
+          var thing = entry.Type.Invoker.CreateInstance(new object[0]);
+        }
+      });
+    }
+
+
+    private static string getDemoName(XamlReader reader) {
+      Debug.Assert(reader.NodeType == XamlNodeType.StartObject);
+      var subReader = reader
+        .ReadSubtree()
+        .ReadMembers()
+        .Where(r => r.Member.DeclaringType.UnderlyingType == typeof(DemoMetadataProperties))
+        .Where(r => r.Member.Name == DemoMetadataProperties.DemoNameProperty.Name)
+        .FirstOrDefault();
+
+      if (subReader != null) {
+        subReader.Read(); // move to value, hopefully
+        if (subReader.NodeType == XamlNodeType.Value) {
+          return (string)subReader.Value;
+        }
+      }
+
+      return null;
+    }
+  }
+
+
+}
