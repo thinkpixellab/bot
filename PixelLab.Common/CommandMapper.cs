ac008a63291503fdd4217aaf798531ad0471a7a4
Kevin Moore <kevin@thinkpixellab.com>
2010-12-06 23:18:31 -0800

    Adding DispatcherExtensions, pulling KeyBinding out into its own class

--- PixelLab.Common/CommandMapper.cs
+++ PixelLab.Common/CommandMapper.cs
@@ -291,41 +291,5 @@ namespace PixelLab.Common
             private readonly Action m_execute;
             private readonly Func<bool> m_canExecute;
         }
-
-        private class KeyBinding : IEquatable<KeyBinding>
-        {
-            public KeyBinding(Key key, ModifierKeys modifierKeys)
-            {
-                Key = key;
-                ModifierKeys = modifierKeys;
-            }
-            public Key Key { get; private set; }
-            public ModifierKeys ModifierKeys { get; private set; }
-
-            public bool Equals(KeyBinding other)
-            {
-                return other != null && other.Key == Key && other.ModifierKeys == ModifierKeys;
-            }
-
-            public override bool Equals(object obj)
-            {
-                return Equals(obj as KeyBinding);
-            }
-
-            public override int GetHashCode()
-            {
-                return Util.GetHashCode(Key, ModifierKeys);
-            }
-
-            public override string ToString()
-            {
-                var output = Key.ToString();
-                (new[] { ModifierKeys.Control, ModifierKeys.Shift, ModifierKeys.Alt, ModifierKeys.Apple, ModifierKeys.Windows })
-                  .Reverse()
-                  .Where(mk => ModifierKeys.HasFlag(mk))
-                  .ForEach(mk => output = "{0}+{1}".DoFormat(mk, output));
-                return output;
-            }
-        }
     }
 }
---
+++ PixelLab.Common/DispatcherExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Threading;
+using System.Diagnostics.Contracts;
+
+namespace PixelLab.Common
+{
+    public static class DispatcherExtensions
+    {
+        public static void Invoke<TResult>(this Dispatcher dispatcher, Func<AsyncCallback, object, IAsyncResult> beginFunction, Func<IAsyncResult, TResult> endFunction, Action<TResult> handler)
+        {
+            beginFunction(asyncResult =>
+            {
+                dispatcher.Invoke(() =>
+                {
+                    var output = endFunction(asyncResult);
+                    handler(output);
+                });
+            }, null);
+        }
+
+        public static void Invoke<T, TResult>(this Dispatcher dispatcher, T param1, Func<T, AsyncCallback, object, IAsyncResult> beginFunction, Func<IAsyncResult, TResult> endFunction, Action<TResult> handler)
+        {
+            beginFunction(param1, asyncResult =>
+            {
+                dispatcher.Invoke(() =>
+                {
+                    var output = endFunction(asyncResult);
+                    handler(output);
+                });
+            }, null);
+        }
+
+        public static void Invoke(this Dispatcher dispatcher, Action action)
+        {
+            Contract.Requires(dispatcher != null);
+            Contract.Requires(action != null);
+
+            var context = new DispatcherSynchronizationContext(dispatcher);
+            context.Send(state =>
+            {
+                action();
+            }, null);
+        }
+    }
+}
