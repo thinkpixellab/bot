ae6422b00b08c9301dc68f8b7e9a60c50d7f1cfb
Kevin Moore <kevin@thinkpixellab.com>
2010-12-16 10:48:21 -0800

    Added ErrorHelper to all
    ...including copies of System.ComponentModel types that aren't in desktop/phone yet

---
+++ PixelLab.Common/System/DataErrorsChangedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace System.ComponentModel
+{
+    public sealed class DataErrorsChangedEventArgs : EventArgs
+    {
+        // Methods
+        public DataErrorsChangedEventArgs(string propertyName)
+        {
+            this.PropertyName = propertyName;
+        }
+
+        // Properties
+        public string PropertyName { get; private set; }
+    }
+}
