9abeea6ad02f1dd4c7242cfab3cc757df3d37450
Kevin Moore <kevin@thinkpixellab.com>
2011-01-28 08:59:19 -0800

    Added DebugTrace -> output to the debugger, even in release build

---
+++ PixelLab.Common/DebugTrace.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+#if CONTRACTS_FULL
+using System.Diagnostics.Contracts;
+#else
+using PixelLab.Contracts;
+#endif
+
+namespace PixelLab.Common
+{
+    public static class DebugTrace
+    {
+        public static void Write(string message)
+        {
+            message = message ?? string.Empty;
+
+            if (Debugger.IsLogging())
+            {
+                Debugger.Log(0, null, message);
+            }
+        }
+
+        public static void Write(object value)
+        {
+            var message = value == null ? string.Empty : value.ToString();
+            Write(message);
+        }
+
+        public static void Write(string format, params object[] args)
+        {
+            Contract.Requires(format != null);
+            Contract.Requires(args != null);
+            Write(format.DoFormat(args));
+        }
+    }
+}
