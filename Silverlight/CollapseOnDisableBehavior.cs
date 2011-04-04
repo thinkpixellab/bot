cfedf095c3e8f16af1da185520cc96fd3c7b8e34
Kevin Moore <kevin@thinkpixellab.com>
2011-04-03 19:48:20 -0700

    Silverlight: double click and collapse-on-disable

---
+++ Silverlight/CollapseOnDisableBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace PixelLab.SL
+{
+    public class CollapseOnDisableBehavior : Behavior<Control>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.IsEnabledChanged += AssociatedObject_IsEnabledChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.IsEnabledChanged -= AssociatedObject_IsEnabledChanged;
+            base.OnDetaching();
+        }
+
+        private void AssociatedObject_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            AssociatedObject.Visibility = AssociatedObject.IsEnabled ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
---
+++ Silverlight/DoubleClickBehavior.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using PixelLab.Contracts;
+
+namespace PixelLab.SL
+{
+    public class DoubleClickBehavior : Behavior<UIElement>
+    {
+        private const int Delta = 250;
+
+        private long _lastDown = int.MinValue;
+
+        public event EventHandler DoubleClick;
+
+        protected virtual void OnDoubleClick(EventArgs e = null)
+        {
+            var handler = DoubleClick;
+            if (handler != null)
+            {
+                handler(this, e ?? EventArgs.Empty);
+            }
+        }
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.AddHandler(UIElement.MouseLeftButtonDownEvent, (MouseButtonEventHandler)target_mouse_down, true);
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.RemoveHandler(UIElement.MouseLeftButtonDownEvent, (MouseButtonEventHandler)target_mouse_down);
+        }
+
+        public static DoubleClickBehavior Get(UIElement element)
+        {
+            Contract.Requires(element != null);
+            return Interaction.GetBehaviors(element).OfType<DoubleClickBehavior>().SingleOrDefault();
+        }
+
+        private void target_mouse_down(object sender, MouseButtonEventArgs e)
+        {
+            var tick = Environment.TickCount;
+            long delta = tick - _lastDown;
+            if (delta < Delta)
+            {
+                OnDoubleClick();
+                _lastDown = int.MinValue;
+            }
+            else
+            {
+                _lastDown = tick;
+            }
+        }
+    }
+}
