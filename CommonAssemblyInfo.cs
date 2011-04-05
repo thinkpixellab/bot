cf29573a2d8a1993ef896f6718953ada13719f6a
Kevin Moore <kevin@thinkpixellab.com>
2011-04-05 13:33:48 -0700

    Cleaned up and added assembly attributes

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#region General information
+[assembly: AssemblyCompany("Pixel Lab")]
+[assembly: AssemblyCopyright("Copyright © 2010, 2011 Pixel Lab. All rights reserved.")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyDescription("https://github.com/thinkpixellab/bot/")]
+
+#if (DEBUG)
+[assembly: AssemblyConfiguration("Debug Build")]
+#else
+[assembly: AssemblyConfiguration("Release Build")]
+#endif
+
+#endregion
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
