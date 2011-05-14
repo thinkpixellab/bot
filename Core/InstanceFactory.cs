f98117e9a28455151c9c234ff6ddc47b97c69df3
Kevin Moore <kevin@thinkpixellab.com>
2011-05-14 13:31:00 -0700

    InstanceFactory: added some details

--- Core/InstanceFactory.cs
+++ Core/InstanceFactory.cs
@@ -4,11 +4,18 @@ namespace PixelLab.Common
 {
     public static class InstanceFactory
     {
+        /// <summary>
+        /// A generic convenience method to create the provided type.
+        /// </summary>
         public static T CreateInstance<T>(params object[] args)
         {
             return (T)typeof(T).CreateInstance(args);
         }
 
+        /// <summary>
+        /// A convenience extension method for Type that calls Activator.CreateInstance
+        /// </summary>
+        /// <returns>A new instance of the provided object.</returns>
         public static object CreateInstance(this Type type, params object[] args)
         {
             return Activator.CreateInstance(type, args);
