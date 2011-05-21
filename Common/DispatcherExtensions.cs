c66a0902a319d8916a65db0239cbcbb0d47f7723
Kevin Moore <kevin@thinkpixellab.com>
2011-05-20 17:58:08 -0700

    DispatcherExtensions: using SynchronizationContext instead of dispatcher -> more general is better

--- Common/DispatcherExtensions.cs
+++ Common/DispatcherExtensions.cs
@@ -4,40 +4,44 @@ using System.Diagnostics.Contracts;
 #else
 using PixelLab.Contracts;
 #endif
-using System.Windows.Threading;
+using System.Threading;
 
 namespace PixelLab.Common
 {
     public static class DispatcherExtensions
     {
-        public static IAsyncResult Invoke<TResult>(this Dispatcher dispatcher, Func<AsyncCallback, object, IAsyncResult> beginFunction, Func<IAsyncResult, TResult> endFunction, Action<TResult> handler, Action<Exception> exceptionHandler)
+        public static IAsyncResult Invoke<TResult>(this SynchronizationContext syncContext, Func<AsyncCallback, object, IAsyncResult> beginFunction, Func<IAsyncResult, TResult> endFunction, Action<TResult> handler, Action<Exception> exceptionHandler)
         {
             return beginFunction(asyncResult =>
             {
-                dispatcher.InvokeHandleCatch(asyncResult, endFunction, handler, exceptionHandler);
+                InvokeHandleCatch(syncContext, asyncResult, endFunction, handler, exceptionHandler);
             }, null);
         }
 
-        public static IAsyncResult Invoke<T, TResult>(this Dispatcher dispatcher, T param1, Func<T, AsyncCallback, object, IAsyncResult> beginFunction, Func<IAsyncResult, TResult> endFunction, Action<TResult> handler, Action<Exception> exceptionHandler)
+        public static IAsyncResult Invoke<T, TResult>(this SynchronizationContext syncContext, T param1, Func<T, AsyncCallback, object, IAsyncResult> beginFunction, Func<IAsyncResult, TResult> endFunction, Action<TResult> handler, Action<Exception> exceptionHandler)
         {
             return beginFunction(param1, asyncResult =>
             {
-                dispatcher.InvokeHandleCatch(asyncResult, endFunction, handler, exceptionHandler);
+                InvokeHandleCatch(syncContext, asyncResult, endFunction, handler, exceptionHandler);
             }, null);
         }
 
-        public static IAsyncResult Invoke<T1, T2, TResult>(this Dispatcher dispatcher, T1 param1, T2 param2, Func<T1, T2, AsyncCallback, object, IAsyncResult> beginFunction, Func<IAsyncResult, TResult> endFunction, Action<TResult> handler, Action<Exception> exceptionHandler)
+        public static IAsyncResult Invoke<T1, T2, TResult>(this SynchronizationContext syncContext, T1 param1, T2 param2, Func<T1, T2, AsyncCallback, object, IAsyncResult> beginFunction, Func<IAsyncResult, TResult> endFunction, Action<TResult> handler, Action<Exception> exceptionHandler)
         {
             return beginFunction(param1, param2, asyncResult =>
             {
-                dispatcher.InvokeHandleCatch(asyncResult, endFunction, handler, exceptionHandler);
+                InvokeHandleCatch(syncContext, asyncResult, endFunction, handler, exceptionHandler);
             }, null);
         }
 
-        // TODO: add logic for 'critical' exception - out of memory, stack overflow, etc
-        private static void InvokeHandleCatch<TResult>(this Dispatcher dispatcher, IAsyncResult asyncResult, Func<IAsyncResult, TResult> endFunction, Action<TResult> handler, Action<Exception> exceptionHandler)
+        private static void InvokeHandleCatch<TResult>(SynchronizationContext synchronizationContext, IAsyncResult asyncResult, Func<IAsyncResult, TResult> endFunction, Action<TResult> handler, Action<Exception> exceptionHandler)
         {
-            dispatcher.Invoke(() =>
+            Contract.Requires(synchronizationContext != null);
+            Contract.Requires(endFunction != null);
+            Contract.Requires(handler != null);
+            Contract.Requires(exceptionHandler != null);
+
+            synchronizationContext.Send((state) =>
             {
                 var success = false;
                 TResult output = default(TResult);
@@ -48,6 +52,10 @@ namespace PixelLab.Common
                 }
                 catch (Exception ex)
                 {
+                    if (ex.IsCriticalException())
+                    {
+                        throw;
+                    }
                     exceptionHandler(ex);
                 }
                 // THIS IS IMPORTANT
@@ -57,18 +65,6 @@ namespace PixelLab.Common
                 {
                     handler(output);
                 }
-            });
-        }
-
-        private static void Invoke(this Dispatcher dispatcher, Action action)
-        {
-            Contract.Requires(dispatcher != null);
-            Contract.Requires(action != null);
-
-            var context = new DispatcherSynchronizationContext(dispatcher);
-            context.Send(state =>
-            {
-                action();
             }, null);
         }
     }
