f30930c34ec585a83c1738c6d9ebd9b7560b2695
Kevin Moore <kevin@thinkpixellab.com>
2011-05-22 13:52:06 -0700

    Silverlight tests: move TestChangeable to its own file

---
+++ Test/Silverlight/Helpers/TestChangeable.cs
@@ -0,0 +1,27 @@
+﻿using PixelLab.Common;
+
+namespace PixelLab.Test.Helpers
+{
+    internal class TestChangeable : Changeable
+    {
+        private int _foo, _bar, _baz;
+
+        public int Foo
+        {
+            get { return _foo; }
+            set { UpdateProperty("Foo", ref _foo, value); }
+        }
+
+        public int Bar
+        {
+            get { return _bar; }
+            set { UpdateProperty("Foo", ref _bar, value); }
+        }
+
+        public int Baz
+        {
+            get { return _baz; }
+            set { UpdateProperty("Baz", ref _baz, value); }
+        }
+    }
+}
--- Test/Silverlight/PropertyWatcherTest.cs
+++ Test/Silverlight/PropertyWatcherTest.cs
@@ -63,27 +63,4 @@ namespace PixelLab.Test
         }
 
     }
-
-    internal class TestChangeable : Changeable
-    {
-        private int _foo, _bar, _baz;
-
-        public int Foo
-        {
-            get { return _foo; }
-            set { UpdateProperty("Foo", ref _foo, value); }
-        }
-
-        public int Bar
-        {
-            get { return _bar; }
-            set { UpdateProperty("Foo", ref _bar, value); }
-        }
-
-        public int Baz
-        {
-            get { return _baz; }
-            set { UpdateProperty("Baz", ref _baz, value); }
-        }
-    }
 }
