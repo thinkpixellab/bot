a1dd7484ce0cf6ead9f02c4723b277dadbe9427c
Kevin Moore <kevin@thinkpixellab.com>
2011-07-07 11:03:31 -0700

    Silverlight Demo: added demo for DoubleClickBehavior
    ...also added in Prism for DelegateCommand

---
+++ Demo/Silverlight/DoubleClickBehaviorPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Practices.Prism.Commands;
+using PixelLab.Demo.Core;
+
+namespace PixelLab.SL.Demo
+{
+    [DemoMetadata("DoubleClickBehavior")]
+    public partial class DoubleClickBehaviorPage : UserControl
+    {
+        public DoubleClickBehaviorPage()
+        {
+            DataContext = new DelegateCommand<string>((param) =>
+            {
+                MessageBox.Show(param, "The double click command has been fired.", MessageBoxButton.OK);
+            });
+
+            InitializeComponent();
+        }
+    }
+}
