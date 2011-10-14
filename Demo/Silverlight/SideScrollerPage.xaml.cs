ef56993d0a8175bb582dbf68fcc7c4610adca9a1
Kevin Moore <kevin@thinkpixellab.com>
2011-10-14 15:42:40 -0700

    ScrollBehavior: let's you wire up list box scroll outside of the list box
    a work in progress

---
+++ Demo/Silverlight/SideScrollerPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using Microsoft.Practices.Prism.Commands;
+using PixelLab.Common;
+using PixelLab.Demo.Core;
+
+namespace PixelLab.SL.Demo
+{
+    [DemoMetadata("Side Scroll")]
+    public partial class SideScrollerPage : UserControl
+    {
+        public SideScrollerPage()
+        {
+            InitializeComponent();
+
+            rightButton.Command = scrollBehavior.ScrollRightCommand;
+            leftButton.Command = scrollBehavior.ScrollLeftCommand;
+        }
+    }
+
+}
---
+++ Silverlight/ScrollBehavior.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using Microsoft.Practices.Prism.Commands;
+using PixelLab.Common;
+
+namespace PixelLab.SL
+{
+    public class ScrollBehavior : Behavior<ListBox>
+    {
+        private readonly DelegateCommand _scrollRightCommand, _scrollLeftCommand;
+
+        private ListBox _listBox;
+        private ScrollViewer _scrollViewer;
+        private ScrollContentPresenter _contentPresenter;
+
+        public ScrollBehavior()
+        {
+            _scrollRightCommand = new DelegateCommand(scrollRight, canScrollRight);
+            _scrollLeftCommand = new DelegateCommand(scrollLeft, canScrollLeft);
+        }
+
+        public ICommand ScrollRightCommand
+        {
+            get
+            {
+                return _scrollRightCommand;
+            }
+        }
+
+        public ICommand ScrollLeftCommand
+        {
+            get
+            {
+                return _scrollLeftCommand;
+            }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            processVisualTree(AssociatedObject);
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            processVisualTree(null);
+        }
+
+        private void scrollRight()
+        {
+            Debug.Assert(canScrollRight());
+            _contentPresenter.LineRight();
+            updateCommands();
+        }
+
+        private bool canScrollRight()
+        {
+            if (_contentPresenter == null)
+            {
+                return false;
+            }
+
+            var max = _contentPresenter.ExtentWidth - _contentPresenter.ViewportWidth;
+            return _contentPresenter.CanHorizontallyScroll && _contentPresenter.HorizontalOffset < max;
+        }
+
+        private void scrollLeft()
+        {
+            Debug.Assert(canScrollLeft());
+            _contentPresenter.LineLeft();
+            updateCommands();
+        }
+
+        private bool canScrollLeft()
+        {
+            return _contentPresenter != null && _contentPresenter.CanHorizontallyScroll && _contentPresenter.HorizontalOffset > 0;
+        }
+
+        private void processVisualTree(ListBox listBox)
+        {
+            if (listBox != _listBox)
+            {
+                if (_listBox != null)
+                {
+                    _listBox.LayoutUpdated -= childLayoutUpdated;
+                }
+                _listBox = listBox;
+                if (_listBox != null)
+                {
+                    _listBox.LayoutUpdated += childLayoutUpdated;
+                }
+            }
+
+            ScrollViewer foundScrollViewer = null;
+            if (_listBox != null)
+            {
+                foundScrollViewer = _listBox.GetVisualDescendents().OfType<ScrollViewer>().SingleOrDefault();
+            }
+
+            if (foundScrollViewer != _scrollViewer)
+            {
+                if (_scrollViewer != null)
+                {
+                    _scrollViewer.LayoutUpdated -= childLayoutUpdated;
+                }
+                _scrollViewer = foundScrollViewer;
+                if (_scrollViewer != null)
+                {
+                    _scrollViewer.LayoutUpdated += childLayoutUpdated;
+                }
+            }
+
+            ScrollContentPresenter foundCP = null;
+            if (_scrollViewer != null)
+            {
+                foundCP = _scrollViewer.GetVisualDescendents().OfType<ScrollContentPresenter>().Where(cp => cp.Name == "ScrollContentPresenter").FirstOrDefault();
+            }
+            if (foundCP != _contentPresenter)
+            {
+                if (_contentPresenter != null)
+                {
+                    _contentPresenter.LayoutUpdated -= childLayoutUpdated;
+                }
+                _contentPresenter = foundCP;
+                if (_contentPresenter != null)
+                {
+                    Debug.Assert(_contentPresenter.ScrollOwner == _scrollViewer);
+                    _contentPresenter.LayoutUpdated -= childLayoutUpdated;
+                }
+            }
+
+            updateCommands();
+        }
+
+        private void updateCommands()
+        {
+            _scrollRightCommand.RaiseCanExecuteChanged();
+            _scrollLeftCommand.RaiseCanExecuteChanged();
+        }
+
+        private void childLayoutUpdated(object sender, EventArgs e)
+        {
+            processVisualTree(this._listBox);
+        }
+    }
+}
