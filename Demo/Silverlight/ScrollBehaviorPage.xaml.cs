7579ab262c66f4a03666f14cf1b3af7a4e25987c
Kevin Moore <kevin@thinkpixellab.com>
2011-10-17 10:01:42 -0700

    ScrollBehaviorPage: naming, binding, etc

---
+++ Demo/Silverlight/ScrollBehaviorPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+using PixelLab.Common;
+using PixelLab.Demo.Core;
+
+namespace PixelLab.SL.Demo
+{
+    [DemoMetadata("ScrollBehavior", "A behavior to attach to a ScrollViewer to expose scroll direction commands.")]
+    public partial class ScrollBehaviorPage : UserControl
+    {
+        public ScrollBehaviorPage()
+        {
+            InitializeComponent();
+
+            _demoListBox.ItemsSource = Enumerable.Range(1, 10).Select(i => "Item {0}".DoFormat(i)).ToList();
+        }
+    }
+}
