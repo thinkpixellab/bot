7af30e2188fe5fc4a3c6580d6a1e99f6ce01a65e
Kevin Moore <kevin@thinkpixellab.com>
2012-05-24 14:58:40 -0700

    v6.0
    lazy about changelog update :-/

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Pixel Lab")]
 [assembly: AssemblyCopyright("Copyright © 2010, 2011 Pixel Lab. All rights reserved.")]
-[assembly: AssemblyVersion("5.2.0")]
-[assembly: AssemblyFileVersion("5.2.0")]
+[assembly: AssemblyVersion("6.0.0")]
+[assembly: AssemblyFileVersion("6.0.0")]
 [assembly: AssemblyDescription("https://github.com/thinkpixellab/bot/")]
 
 #if (DEBUG)
