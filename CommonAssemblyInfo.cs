7332bb8d8e29964e980df34e864cf7636acb4b96
Kevin Moore <kevin@thinkpixellab.com>
2012-06-05 16:09:06 -0700

    v6.0.1
    Moving around projects and supporting SL5

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Pixel Lab")]
 [assembly: AssemblyCopyright("Copyright © 2010, 2011 Pixel Lab. All rights reserved.")]
-[assembly: AssemblyVersion("6.0.0")]
-[assembly: AssemblyFileVersion("6.0.0")]
+[assembly: AssemblyVersion("6.0.1")]
+[assembly: AssemblyFileVersion("6.0.1")]
 [assembly: AssemblyDescription("https://github.com/thinkpixellab/bot/")]
 
 #if (DEBUG)
